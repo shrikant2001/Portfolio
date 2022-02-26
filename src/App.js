0ce83e0a568f8c8eb0c22bef15876ce956a487c2
shrikant jadhav <shrikantjadhav20@cse.iiitp.ac.in>
2022-02-26 19:52:21 +0530

    base

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* navbar */}
+      <Navbar/>
+      
+      {/* Main */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Education and expirience */}
+
+      {/* aout and contact */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Styles/Navbar.scss'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>Navbar</div>
+  )
+}
+
+export default Navbar
